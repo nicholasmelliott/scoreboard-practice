4023a95bae0afae48ca94b88aed0fa8b30789873
Nick Elliott <nicholasmelliott@yahoo.com>
2018-11-04 16:46:58 -0500

    Changed the stateless functional component Counter to a class so that state can be added

--- app.js
+++ app.js
@@ -30,14 +30,16 @@ const Header = (props) => {
   );
 }
 
-const Counter = (props) => {
+class Counter extends React.Component {
+  render() {
     return (
     <div className="counter">
       <button className="counter-action decrement"> - </button>
-      <span className="counter-score">{props.score}</span>
+      <span className="counter-score">{this.props.score}</span>
       <button className="counter-action increment"> + </button>
     </div>
     )
+  }
 }
 
 const Player = (props) => {
