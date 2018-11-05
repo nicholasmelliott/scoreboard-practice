098733753aed7056f4b35065f98c199b7d67b4fc
Nick Elliott <nicholasmelliott@yahoo.com>
2018-11-04 20:14:49 -0500

    Added handleRemovePlayer method to App component which deletes player when x btn is clicked by name.

--- app.js
+++ app.js
@@ -44,6 +44,7 @@ const Player = (props) => {
   return (
     <div className="player">
       <span className="player-name">
+        <button className="remove-player" onClick={ () => props.removePlayer(props.id)}>✖</button>
         {props.name}
       </span>
       <Counter />
@@ -78,6 +79,12 @@ class App extends React.Component {
     }
   }
 
+  handleRemovePlayer = (id) => {
+    this.setState( prevState => ({ 
+      players: prevState.players.filter( p => p.id !== id )
+    }))  
+  }
+
   render(){
     return (
     <div className="scoreboard">
@@ -90,7 +97,9 @@ class App extends React.Component {
       {this.state.players.map( player => 
         <Player 
           name={player.name} 
+          id={player.id}
           key={player.id.toString()}
+          removePlayer={this.handleRemovePlayer}
         />
       )}
       
